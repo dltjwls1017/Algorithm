75fe2f999084f5db174699b6d6a6ffbf6e7c2880
[Github <dltjwls1017@naver.com>
2023-11-01 19:37:55 +0900

    11-01

---
+++ b2_9498.py
@@ -0,0 +1,12 @@
+s = int(input())
+if s > 59 and s < 70:
+    print('D')
+
+elif s > 69 and s < 80: 
+    print('C') 
+elif s > 79 and s < 80:
+    print('B')   
+elif s > 89 and s < 101:
+    print('A')  
+else:
+    print('F')     
